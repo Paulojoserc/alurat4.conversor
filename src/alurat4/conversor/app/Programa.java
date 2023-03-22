fbabd0f8c3cc9c80a09746693c08fe16b725d1ee
Paulo Costa <pjrc83@gmail.com>
2023-03-22 17:56:10 -0300

    Model Moeda

---
+++ src/alurat4/conversor/app/Programa.java
@@ -0,0 +1,5 @@
+package alurat4.conversor.app;
+
+public class Programa {
+
+}
---
+++ src/alurat4/conversor/model/Moeda.java
@@ -0,0 +1,33 @@
+package alurat4.conversor.model;
+
+public class Moeda {
+String code;
+String codein;
+String name;
+double bid;
+
+public String getCodigoMoedaEntrada() {
+    return code;
+}
+
+public String getCodigoMoedaSaida() {
+    return codein;
+}
+
+public String getName() {
+    return name;
+}
+public double getValor() {
+    return bid;
+}
+
+@Override
+public String toString() {
+    return "ConversorMoeda{" +
+            "name='" + name + '\'' +
+            ", codigoMoedaEntrada='" + code + '\'' +
+            ", codigoMoedaSaida='" + codein + '\'' +
+            ", valor=" + bid +
+            '}';
+}
+}
